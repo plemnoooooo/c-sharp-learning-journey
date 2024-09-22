76c75e381434ea532e7382625bd1a9197c153d72
plemno <plemnotl@gmail.com>
2024-09-22 09:35:26 +0000

    part 6

--- Program.cs
+++ Program.cs
@@ -1,8 +1,10 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, Plemno!");
 
-string firstFriend = "       Maria     ";
+string firstFriend = "Maria";
 firstFriend = firstFriend.Trim();
 
-string secondFriend = "   Scott     ";
-Console.WriteLine($"My friends are {firstFriend} and {secondFriend.Trim()}");
+string secondFriend = "Scott";
+string friends = $"My friends are {firstFriend} and {secondFriend.Trim()}";
+
+Console.WriteLine(friends.StartsWith("My "));
