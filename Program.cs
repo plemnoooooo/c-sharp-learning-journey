ad3bca4c0ae201f6698a00816f9a5c3302588c86
plemno <plemnotl@gmail.com>
2024-09-22 13:04:40 +0000

    part 10

--- Program.cs
+++ Program.cs
@@ -1,14 +1,8 @@
-﻿int a = 5;
-int b = 3;
-int c = 4;
+﻿int counter = 10;
 
-if ((a + b + c > 10) || (a == b))
+do
 {
-    Console.WriteLine("The answer is greater than 10");
-    Console.WriteLine("And the first number is equal to the second");
+    Console.WriteLine(counter);
+    counter++;
 }
-else
-{
-    Console.WriteLine("The answer is not greater than 10");
-    Console.WriteLine("Or the first number is not equal to the second");
-}
+while (counter < 5);
