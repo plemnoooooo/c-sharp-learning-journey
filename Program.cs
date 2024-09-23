8f7e013bfd1e667763427894f0c658bc72ea2b1b
plemno <plemnotl@gmail.com>
2024-09-23 05:32:57 +0000

    part 16

--- Program.cs
+++ Program.cs
@@ -1,16 +1,19 @@
 ﻿// Specify the data source.
-List<int> scores = [97, 92, 81, 60];
+List<int> scores = [3, 45, 82, 97, 92, 100, 81, 60];
 
 // Define the query expression.
-IEnumerable<int> scoreQuery =
+IEnumerable<string> scoreQuery =
     from score in scores
     where score > 80
-    select score;
+    orderby score descending
+    select $" The score is {score}";
+
+Console.WriteLine(scoreQuery.Count());
 
 // Execute the query.
-foreach (int i in scoreQuery)
+foreach (string s in scoreQuery)
 {
-    Console.Write(i + " ");
+    Console.WriteLine(s);
 }
 
 // Output: 97 92 81 
